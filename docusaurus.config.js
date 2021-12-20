0af95d5f417a0956a25dbad4c8fc5760cd0a2530
Lotte <>
2021-12-21 00:22:37 +0100

    Using docusaurus for static website generation

---
+++ docusaurus.config.js
@@ -0,0 +1,127 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+	title: 'Arrow',
+	tagline: 'Bringing Private Air Travel to Everyone',
+	url: 'https://arrowair.com',
+	baseUrl: '/',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
+	favicon: 'img/favicon.ico',
+	organizationName: 'Arrow', // Usually your GitHub org/user name.
+	projectName: 'arrow', // Usually your repo name.
+
+	stylesheets: [
+		{
+			href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
+			integrity: 'sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3', 
+			crossorigin: 'anonymous'
+		},
+		{ href: 'https://fonts.googleapis.com', rel: 'preconnect' },
+		{ href: 'https://fonts.gstatic.com', rel: 'preconnect' },
+		{ href: 'https://fonts.googleapis.com/css2?family=Karla:wght@400;700&family=Rubik:wght@400;700&display=swap' }
+	],
+
+	presets: [
+		[
+			'classic',
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					sidebarPath: require.resolve('./sidebars.js'),
+					// Please change this to your repo.
+					editUrl: 'https://github.com/Arrow-air/wiki/edit/main/website/',
+				},
+				blog: {
+					showReadingTime: true,
+					// Please change this to your repo.
+					editUrl:
+					'https://github.com/Arrow-air/wiki/edit/main/website/blog/',
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			}),
+		],
+	],
+
+	themeConfig:
+	/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+	({
+		colorMode: {
+			disableSwitch: true
+		},
+		navbar: {
+			logo: {
+				alt: 'Arrow Logo',
+				src: 'img/wordmark_gray.svg',
+			},
+			items: [
+				{
+					type: 'doc',
+					docId: 'intro',
+					position: 'right',
+					label: 'Pages',
+				},
+				{to: '/blog', label: 'Blog', position: 'right'},
+				{
+					href: 'https://discord.com/invite/fab4bxaAW9',
+					label: 'Discord',
+					position: 'right',
+					className: 'px-3 bg-navy text-white',
+				},
+			],
+		},
+		footer: {
+			style: 'light',
+			links: [
+				{
+					title: 'Docs',
+					items: [
+						{
+							label: 'Intro',
+							to: '/docs/intro',
+						},
+					],
+				},
+				{
+					title: 'Community',
+					items: [
+						{
+							label: 'Discord',
+							href: 'https://discord.com/invite/fab4bxaAW9',
+						},
+						{
+							label: 'Twitter',
+							href: 'https://twitter.com/docusaurus',
+						},
+					],
+				},
+				{
+					title: 'More',
+					items: [
+						{
+							label: 'Blog',
+							to: '/blog',
+						},
+						{
+							label: 'GitHub',
+							href: 'https://github.com/Arrow-air',
+						},
+					],
+				},
+			],
+		},
+		prism: {
+			theme: lightCodeTheme,
+			darkTheme: darkCodeTheme,
+		},
+	}),
+};
+
+module.exports = config;
---
+++ src/components/Ecosystem.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import clsx from 'clsx';
+
+export default function Ecosystem(): JSX.Element {
+	return (
+		<React.Fragment>
+			<article className="row justify-content-center text-center bg-white text-blue-dark bg-ecosystem border-top border-lavender-light">
+				<div className="col-10 col-xl-5 pt-3">
+					<div className="row justify-content-center my-5">
+						<img src="img/div_eco.svg" className="col-4 col-sm-3 col-md-2 col-xl-3 img-fluid" alt="Ecosystem"/>
+					</div>
+
+					<div className="row justify-content-center mb-3">
+						<h3 className="mb-5 text-nowrap">Our ecosystem</h3>
+						<p className="fs-5">
+						The <strong>Arrow</strong> protocol will bring together builders, riders, operators, and many more in a way never before possible.
+						</p>
+					</div>
+
+					<div className="row justify-content-center row-cols-2 row-cols-sm-2 row-cols-md-3 g-5 fs-7 mb-5">
+						<div className="col">
+							<div className="card h-100">
+								<div className="row card-body text-center px-2">
+									<h5 className="fs-6 col-12 order-2 order-md-1 py-3 card-text">Vertical Takeoff Aircraft</h5>
+									<div className="col-10 order-1 order-lg-2">
+										<img src="img/vtol@2x.png" className="img-fluid" alt="Vertical Takeoff Aircraft"/>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="col">
+							<div className="card h-100">
+								<div className="row card-body text-center px-2">
+									<h5 className="fs-6 col-12 order-2 order-md-1 py-3 card-text">Point to Point</h5>
+									<div className="col-10 order-1 order-lg-2">
+										<img src="img/p2p@2x.png" className="img-fluid" alt="Point to Point"/>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="col">
+							<div className="card h-100">
+								<div className="row card-body text-center px-2">
+									<h5 className="fs-6 col-12 order-2 order-md-1 py-3 card-text">Connect Humans</h5>
+									<div className="col-10 order-1 order-md-2">
+										<img src="img/connect@2x.png" className="img-fluid" alt="Connect humans"/>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="col-md-9 d-none d-md-block mt-0">
+							<div className="card h-100">
+								<div className="card-body text-center p-0">
+									<img src="img/spider-diagram.png" className="img-fluid"/>
+								</div>
+							</div>
+						</div>
+						<div className="col mt-md-0">
+							<div className="card h-100">
+								<div className="card-body text-center px-2 row">
+									<div className="col-10">
+										<img src="img/rideshare@2x.png" className="img-fluid" alt="Rideshare Protocol"/>
+									</div>
+									<h5 className="fs-6 py-3 card-text">Rideshare Protocol</h5>
+								</div>
+							</div>
+						</div>
+						<div className="col mt-md-0">
+							<div className="card h-100">
+								<div className="card-body text-center px-2 row">
+									<div className="col-10">
+										<img src="img/global@2x.png" className="img-fluid" alt="Global Team"/>
+									</div>
+									<h5 className="fs-6 py-3 card-text text-nowrap">Global Team</h5>
+								</div>
+							</div>
+						</div>
+						<div className="col mt-md-0">
+							<div className="card h-100">
+								<div className="card-body text-center px-2 row">
+									<div className="col-10">
+										<img src="img/collab@2x.png" className="img-fluid" alt="Collaborative"/>
+									</div>
+									<h5 className="fs-6 py-3 card-text text-nowrap">Collaborative</h5>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</article>	
+		</React.Fragment>
+	);
+}
