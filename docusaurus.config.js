0c76217f41cf04c00a5603e1a77548ab69522be9
Lotte <95369289+owlot@users.noreply.github.com>
2022-02-16 19:21:27 +0100

    Staging (#8)
    * fix the Twitter link; reposition the dark mode toggle button
    * Create seperate branch for staging so we can have PR's merged and tested here if they come from a fork
    
    Co-authored-by: Xipu Li <lixipu98@gmail.com>
    Co-authored-by: Lotte <>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,135 +1,138 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: 'Arrow',
-	tagline: 'Bringing Private Air Travel to Everyone',
-	url: 'https://arrowair.com',
-	baseUrl: '/',
-	onBrokenLinks: 'throw',
-	onBrokenMarkdownLinks: 'warn',
-	favicon: 'img/favicon.ico',
-	organizationName: 'Arrow', // Usually your GitHub org/user name.
-	projectName: 'arrow', // Usually your repo name.
+  title: "Arrow",
+  tagline: "Bringing Private Air Travel to Everyone",
+  url: "https://arrowair.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "Arrow", // Usually your GitHub org/user name.
+  projectName: "arrow", // Usually your repo name.
 
-	stylesheets: [
-		{
-			href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
-			integrity: 'sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3', 
-			crossorigin: 'anonymous'
-		},
-		{ href: 'https://fonts.googleapis.com', rel: 'preconnect' },
-		{ href: 'https://fonts.gstatic.com', rel: 'preconnect' },
-		{ href: 'https://fonts.googleapis.com/css2?family=Karla:wght@400;700&family=Rubik:wght@400;700&display=swap' }
-	],
+  stylesheets: [
+    {
+      href: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+      integrity:
+        "sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3",
+      crossorigin: "anonymous",
+    },
+    { href: "https://fonts.googleapis.com", rel: "preconnect" },
+    { href: "https://fonts.gstatic.com", rel: "preconnect" },
+    {
+      href: "https://fonts.googleapis.com/css2?family=Karla:wght@400;700&family=Rubik:wght@400;700&display=swap",
+    },
+  ],
 
-	presets: [
-		[
-			'classic',
-			/** @type {import('@docusaurus/preset-classic').Options} */
-			({
-				docs: {
-					sidebarPath: require.resolve('./sidebars.js'),
-					// Please change this to your repo.
-					editUrl: 'https://github.com/Arrow-air/wiki/edit/main/website/',
-				},
-				blog: {
-					showReadingTime: true,
-					// Please change this to your repo.
-					editUrl:
-					'https://github.com/Arrow-air/wiki/edit/main/website/blog/',
-				},
-				theme: {
-					customCss: require.resolve('./src/css/custom.css'),
-				},
-			}),
-		],
-	],
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl: "https://github.com/Arrow-air/wiki/edit/main/website/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl: "https://github.com/Arrow-air/wiki/edit/main/website/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
 
-	themeConfig:
-	/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-	({
-		colorMode: {
-			disableSwitch: true
-		},
-		navbar: {
-			logo: {
-				alt: 'Arrow Logo',
-				src: 'img/wordmark_gray.svg',
-			},
-			items: [
-				{
-					type: 'doc',
-					docId: 'intro',
-					position: 'right',
-					label: 'Docs',
-					className: 'text-secondary',
-				},
-				{
-					to: '/blog',
-					position: 'right',
-					label: 'Blog',
-					className: 'text-secondary',
-					image: 'img/arrow-icon_blog.svg',
-				},
-				{
-					href: 'https://discord.com/invite/fab4bxaAW9',
-					position: 'right',
-					label: 'Discord',
-					className: 'text-white',
-					image: 'img/arrow-icon_discord.svg',
-				},
-			],
-		},
-		footer: {
-			style: 'light',
-			logo: {
-				alt: 'Arrow Logo',
-				src: 'img/wordmark_gray.svg',
-			},
-			copyright: 'Built with <img src="/img/arrow-icon_love.svg" style="height:1rem" alt="love"/> by the Arrow Community',
-			links: [
-				{
-					title: 'Community',
-					items: [
-						{
-							label: 'Discord',
-							href: 'https://discord.com/invite/fab4bxaAW9',
-						},
-						{
-							label: 'Twitter',
-							href: 'https://twitter.com/docusaurus',
-						},
-					],
-				},
-				{
-					title: 'Links',
-					items: [
-						{
-							label: 'Intro',
-							to: '/docs/intro',
-						},
-						{
-							label: 'Blog',
-							to: '/blog',
-						},
-						{
-							label: 'GitHub',
-							href: 'https://github.com/Arrow-air',
-						},
-					],
-				},
-			],
-		},
-		prism: {
-			theme: lightCodeTheme,
-			darkTheme: darkCodeTheme,
-		},
-	}),
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        disableSwitch: true,
+      },
+      navbar: {
+        logo: {
+          alt: "Arrow Logo",
+          src: "img/wordmark_gray.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "right",
+            label: "Docs",
+            className: "text-secondary",
+          },
+          {
+            to: "/blog",
+            position: "right",
+            label: "Blog",
+            className: "text-secondary",
+            image: "img/arrow-icon_blog.svg",
+          },
+          {
+            href: "https://discord.com/invite/fab4bxaAW9",
+            position: "right",
+            label: "Discord",
+            className: "text-white",
+            image: "img/arrow-icon_discord.svg",
+          },
+        ],
+      },
+      footer: {
+        style: "light",
+        logo: {
+          alt: "Arrow Logo",
+          src: "img/wordmark_gray.svg",
+        },
+        copyright:
+          'Built with <img src="/img/arrow-icon_love.svg" style="height:1rem" alt="love"/> by the Arrow Community',
+        links: [
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Discord",
+                href: "https://discord.com/invite/fab4bxaAW9",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/ArrowAir_",
+              },
+            ],
+          },
+          {
+            title: "Links",
+            items: [
+              {
+                label: "Intro",
+                to: "/docs/intro",
+              },
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/Arrow-air",
+              },
+            ],
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
 };
 
 module.exports = config;
