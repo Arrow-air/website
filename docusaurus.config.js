fd6b6f1b4d3cb8257c81b0c4ae41bfcbeaca3745
Lotte <unknown>
2022-04-25 16:57:25 +0200

    Added static website files from webflow export

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,13 +37,11 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          editUrl: "https://github.com/Arrow-air/wiki/edit/main/website/",
+          editUrl: 'https://github.com/Arrow-air/website/edit/main/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          editUrl: "https://github.com/Arrow-air/wiki/edit/main/website/blog/",
+          editUrl: 'https://github.com/Arrow-air/website/edit/main/',
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
