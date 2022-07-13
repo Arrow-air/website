b0175f6a8da75239823b04ab199c0878e38ce455
AM Smith <smitham42@gmail.com>
2022-07-13 12:26:00 -0700

    Add datatable Glossary search

---
+++ docs/documentation/GlossaryData.js
@@ -0,0 +1,20 @@
+export const glossaryData = [
+    [
+        // Name
+        "Arrow",
+
+        // Description
+        `Arrow is a decentralized community building open source vertical-takeoff aircraft"
+        and a rideshare protocol that will bring point to point air travel to everyone.`,
+
+        // Dictionary of links
+        "https://www.arrowair.com/"
+    ],
+
+    [
+        "VTOL",
+        `Vertical Takeoff and Landing Aircraft. eVTOL == electric VTOL.`,
+        "https://en.wikipedia.org/wiki/VTOL"
+    ],
+
+]
