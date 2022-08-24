03bc96b5dde292cc0e059468e07744b7d5ce3364
A.M. Smith <ServiceDog@users.noreply.github.com>
2022-08-24 10:43:22 -0700

    Add UAM, SDSP, PSU, PIC

--- docs/documentation/GlossaryData.js
+++ docs/documentation/GlossaryData.js
@@ -17,6 +17,25 @@ export const data = [
         ["https://www.arrowair.com/"]
     ],
     [
+        "PIC",
+        `Pilot in Command: a human operating a VTOL vehicle. RPIC == Remote PIC (a human piloting the vehicle remotely).`,
+        []
+    ],
+    [
+        "PSU - Provider of Services",
+        `An entity that provides notifications and aviation authority confirmations to UAM Operators, enabling safe and secure use of airspace.`,
+        ["https://ntrs.nasa.gov/citations/20210019876"]
+    ],
+    [
+        "SDSP",
+        `Supplemental Data and Services Provider. Provides information to augment UAM operations such as data on terrain, weather, vertiport availability, as so forth.`,
+        ["https://ntrs.nasa.gov/citations/20210019876"]
+    ],
+    [
+        "UAM",
+        `Urban Aerial Mobility. Consists of vehicles, software, and infrastructure supporting urban aerial activity.`,
+        ["https://ntrs.nasa.gov/citations/20210019876"]
+    [
         "VTOL",
         `Vertical Takeoff and Landing Aircraft. eVTOL == electric VTOL.`,
         ["https://en.wikipedia.org/wiki/VTOL"]
